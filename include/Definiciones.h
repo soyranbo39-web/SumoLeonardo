e1b943b5629b15c126ea0378c52d9e6df90ba68b
kevinrosaldo <soy.ranbo39@gmail.com>
2026-05-21 07:42:51 -0600

    sss

--- include/Definiciones.h
+++ include/Definiciones.h
@@ -32,9 +32,9 @@ const int Pin_Control_Remoto = A0; // Start=1, Stop=0
 
 // Parámetros
 #define BLANCO 120
-#define Velocidad_movimiento_seguir 200// 160 
-#define Velocidad_estandar 200 //180  
-#define Velocidad_normal 200  // 120 
+#define Velocidad_movimiento_seguir 160 
+#define Velocidad_estandar 180  
+#define Velocidad_normal 120 
 #define Velocidad_maxima 200
 
 #endif // DEFINICIONES_H
